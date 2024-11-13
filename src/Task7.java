49aac9a726380eb903d0a7613c299c1ee94d281c
ISOMARCO <inagiyev@icloud.com>
2024-11-13 23:51:46 +0400

    Task7

---
+++ src/Task7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        if(num > 0) {
+            System.out.println("Number is positive");
+        } else if (num < 0) {
+            System.out.println("Number is negative");
+        } else {
+            System.out.println("Number is zero");
+        }
+    }
+}
