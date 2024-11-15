3f288f012e52a076d136fe40a26f5f1c4e5a5861
ISOMARCO <inagiyev@icloud.com>
2024-11-15 23:10:14 +0400

    faktorial

---
+++ src/Task16.java
@@ -0,0 +1,21 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Task16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ədəd daxil edin: ");
+        int num = scanner.nextInt();
+        if(num == 0) {
+            System.out.println("1");
+        } else if (num < 0) {
+            System.out.println("Yalnız müsbət ədəd (və ya sıfır) daxil edə bilərsiniz");
+        } else {
+            BigInteger fac = BigInteger.ONE;
+            for(int i = 1; i <= num; i++) {
+                fac = fac.multiply(BigInteger.valueOf(i));
+            }
+            System.out.println(num + "! = " + fac);
+        }
+    }
+}
