518a43ee38f9f9807fdd5fed115e385973573832
ismile <ismayilnagiyev100@gmail.com>
2024-12-20 12:21:44 +0400

    streamexample

--- src/StreamExample3.java
+++ src/StreamExample3.java
@@ -10,10 +10,10 @@ public class StreamExample3 {
     public static void main(String[] args) {
         Integer[] intArray = {5, 15, 20, 25, 2, 8};
         filterElement(intArray, n -> n > 10);
-
+        System.out.println();
         String[] stringArray = {"Ismayil", "Elchin", "Ismayil12345", "ismayil12"};
         filterElement(stringArray, s -> s.contains("Ismayil"));
-
+        System.out.println();
         Double[] doubleArray = {45.5, 60.0, 32.2, 80.1, 50.0};
         filterElement(doubleArray, d -> d < 50.0);
     }
