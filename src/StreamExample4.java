27f8a040d5639bf03be6558fde94d6df37779fb3
ismile <ismayilnagiyev100@gmail.com>
2024-12-20 14:05:32 +0400

    streamexample

--- src/StreamExample4.java
+++ src/StreamExample4.java
@@ -4,7 +4,9 @@ public class StreamExample4 {
     public static void main(String[] args) {
         List<String> strList = Arrays.asList("Ismayil", "Java", "ATL");
         var result = strList.stream()
-                .map(s -> Arrays.stream(s.split("")).collect(Collectors.toList())).flatMap(Collection::stream)
+                .map(s -> Arrays.stream(s.split(""))
+                .collect(Collectors.toList()))
+                .flatMap(Collection::stream)
                 .toList();
         System.out.println(result);
     }
