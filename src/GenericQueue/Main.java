cb13b766cf9971227287dce1438cc6d3c2d8f05e
ismile <ismayilnagiyev100@gmail.com>
2024-12-25 15:16:48 +0400

    generic queue

--- src/GenericQueue/Main.java
+++ src/GenericQueue/Main.java
@@ -1,12 +1,23 @@
 package GenericQueue;
 
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
+        String errMessage = null;
         var queue = new GenericQueue<Integer>();
-        queue.enqueue(1);
-        queue.enqueue(2);
-        System.out.println(queue);
-        queue.dequeue();
-        System.out.println(queue);
+        try {
+            queue.enqueue(1);
+            queue.enqueue(2);
+            queue.dequeue();
+            queue.dequeue();
+            queue.dequeue();
+        }catch(Exception e) {
+            errMessage = e.getMessage();
+        } finally {
+            System.out.println(queue);
+            System.err.println(errMessage != null ? errMessage : "");
+        }
+
     }
 }
